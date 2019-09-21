08653825524bba753e766b10762e98aeda970410
Magnus Gjerde <gjerde.magnus@gmail.com>
2019-09-21 12:06:11 +0200

    Uploaded

--- MainActivity.java
+++ MainActivity.java
@@ -9,22 +9,15 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-
-    private FragmentStatePagerAdapter mFragmentStatePagerAdapter;
     private ViewPager viewPager;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate: Started");
-
         viewPager = findViewById(R.id.view_pager);
-
         setupViewPager(viewPager);
-
-
     }
 
     private void setupViewPager (ViewPager viewPager){
