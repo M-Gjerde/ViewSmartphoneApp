13cee367b5f6f6b6841bdf836e5ba0ce6982e9a3
Magnus Gjerde <gjerde.magnus@gmail.com>
2020-02-04 13:42:44 +0100

    Uploaded

---
+++ utils/Constants.java
@@ -0,0 +1,9 @@
+package net.kaufmanndesigns.view.utils;
+
+public class Constants {
+
+    public static String KEY_EMAIL = "email";
+    public static String KEY_PASSWORD = "password";
+    public static String KEY_MIRROR_ID = "mirror_id";
+    public static String KEY_LOGGED_IN = "logged_in";
+}
